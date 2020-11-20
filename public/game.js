85ad556caab944db09801a45e61606179cbcd94b
Marcsa1 <doubl3omarc@gmail.com>
2020-11-20 13:23:55 -0500

    changed home, server validation, readme

--- public/game.js
+++ public/game.js
@@ -9,7 +9,18 @@ $(document).ready(function () {
   socket.on('connectionAccepted', (player1) => {
     turn = player1;
   });
+
+  socket.on('connectionRejected', () => {
+    alert('That room code is already in use!');
+    window.location.href = '/';
+    localStorage.setItem('roomCode', null);
+  });
   
+  socket.on('selfToggle', (coords) => {
+    $(`#${coords[0]}${coords[1]}`).css('background-color', 'blue');
+    turn = !turn;
+  });
+
   socket.on('opponentToggle', (coords) => {
     turn = !turn;
     $(`#${coords[0]}${coords[1]}`).css('background-color', 'red');
@@ -18,8 +29,5 @@ $(document).ready(function () {
 
 function toggleSquare(x, y) {
   if(!turn) return;
-
-  $(`#${x}${y}`).css('background-color', 'blue');
   socket.emit('toggle', [x,y]);
-  turn = !turn;
 }
